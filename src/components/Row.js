b289176681391ae49703d2c0bbd41154c304777a
gawthamkrishna <gawthamkrishna21@gmail.com>
2023-02-28 17:01:42 +0530

    netflix

---
+++ src/components/Row.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import instance from '../instance'
+import './Row.css'
+
+function Row({isPoster, title, fetchUrl }) {
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+
+  const [movies, setMovies] = useState([])
+
+  const fetchData = async () => {
+    const response = await instance.get(fetchUrl)
+    setMovies(response.data.results);
+  }
+  useEffect(() => {
+    fetchData()
+
+  }, [])
+
+  // console.log(setMovies);
+
+  return (
+    <div className='row'>
+      <h2>{title}</h2>
+      <div className='movies_row'>
+        {
+          movies.map((movie) => (
+            <img className={`movie ${isPoster && 'movie_poster'}`}
+              src={`${base_url}${isPoster?movie.poster_path:movie.backdrop_path}`}
+            />
+          ))
+
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Row
